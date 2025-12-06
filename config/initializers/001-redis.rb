503865c7ee9aeb58b4016c0cbea92d0bedd3b7c9
parkingguardian <parkingguardian1@gmail.com>
2025-12-06 16:15:04 +0000

    Update 001-redis.rb

--- config/initializers/001-redis.rb
+++ config/initializers/001-redis.rb
@@ -14,18 +14,3 @@ if defined?(Sidekiq)
   end
 end
 
-# Flush Redis in development mode if requested
-if Rails.env.development? && ENV["DISCOURSE_FLUSH_REDIS"]
-  puts "Flushing redis (development mode)"
-  Discourse.redis.flushdb
-end
-
-# Verify Redis version
-begin
-  if Gem::Version.new(Discourse.redis.info["redis_version"]) < Gem::Version.new("6.2.0")
-    STDERR.puts "Discourse requires Redis 6.2.0 or up"
-    exit 1
-  end
-rescue Redis::CannotConnectError
-  STDERR.puts "Couldn't connect to Redis at #{ENV["REDIS_URL"]}"
-end
